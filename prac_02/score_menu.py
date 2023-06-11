b397c7c8ea5a94521180eae819f4a9fbefba37e9
KalynAnne <kalyn.barry@my.jcu.edu.au>
2023-06-11 16:58:08 +1000

    Add functions to determine grade and print stars according to grade. Fixed problems with float/int and saved the results from get_valid_score

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -6,17 +6,16 @@ MENU = """(G)et a valid score
 
 def main():
     print("To start please enter a score")
-    get_valid_score()
+    score = get_valid_score()
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            get_valid_score()
-            pass
+            score = get_valid_score()
         elif choice == "P":
-            pass
+            print(determine_grade_result(score))
         elif choice == "S":
-            pass
+            print_stars(score)
         else:
             print("Invalid option")
         print(MENU)
@@ -25,11 +24,26 @@ def main():
 
 
 def get_valid_score():
-    score = float(input("Enter score: "))
+    score = int(input("Enter score: "))
     while 100 < score > 0:
         print("Invalid score")
-        score = float(input("Enter score: "))
+        score = int(input("Enter score: "))
     return score
+
+
+def determine_grade_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def print_stars(score):
+    print("*" * score)
 
 
 main()
