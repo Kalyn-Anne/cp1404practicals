5e682a1cfee0e96e2c793cbc856e00725e055765
KalynAnne <kalyn.barry@my.jcu.edu.au>
2023-06-18 18:58:57 +1000

    Added while loop for denominator, removed except ZeroDivisionError

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -6,16 +6,18 @@ When the value of the input is not an int.
 2. When will a ZeroDivisionError occur?
 When the denominator input is 0, as division is impossible.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
-Yes, we could use a while loop to get a valid number without using except.
+Yes, we could use a while loop to get a valid number without using except. This allows the user to
+keep entering until something other than 0 is entered rather than end the code.
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Cannot divide by zero!")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
 print("Finished.")
