e36eda00247eb2f9a32efa27ef763e6bfd30a675
KalynAnne <kalyn.barry@my.jcu.edu.au>
2023-06-25 13:52:40 +1000

    Complete Woefully inadequate security checker

--- prac_04/list_excersises.py
+++ prac_04/list_excersises.py
@@ -9,3 +9,12 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the number is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter your username: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
