3651e9c5fd288727d5bbe781867427f225b41899
KalynAnne <kalyn.barry@my.jcu.edu.au>
2023-07-02 20:19:07 +1000

    ADD 10 colour and code dictionary items

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Practical
+Program that allows you to look up hexadecimal colour,
+Entering an invalid colour name should not crash the program.
+Allow the user to enter names until they enter a blank one to stop the loop.
+"""
+
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Asparagus": "#87a96b", "Beaver": "#9f8170", "Black Coffee": "#3b2f2f",
+                  "Bone": "#e3dac9", "Boysenberry": "#873260", "Bubble Gum": "#ffc1cc", "Citron": "#9fa91f",
+                  "Corn": "#fbec5d", "#Eggplant": "#614051"}
+print(COLOUR_TO_CODE)
+print(len(COLOUR_TO_CODE))
