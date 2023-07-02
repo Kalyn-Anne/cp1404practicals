9221845bba5103648c17b50b15b02400fa069802
KalynAnne <kalyn.barry@my.jcu.edu.au>
2023-07-02 22:02:58 +1000

    Add process function to find winning countries and count champion win count. Add basic print to check process function.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,31 +2,42 @@
 CP1404 Practical
 """
 FILENAME = "wimbledon"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
 
 
 def main():
-    file_information = get_file_information(FILENAME)
-    print(file_information)
-    process_file_information()
-    display_results()
+    records = get_records(FILENAME)
+    print(records)
+    champion_to_count, countries = process_records(records)
+    display_results(champion_to_count, countries)
 
 
-def process_file_information():
-    pass
+def process_records(records):
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[COUNTRY_INDEX])
+        try:
+            champion_to_count[record[CHAMPION_INDEX]] += 1
+        except KeyError:
+            champion_to_count[record[CHAMPION_INDEX]] = 1
+    return champion_to_count, countries
 
 
-def display_results():
-    pass
+def display_results(champion_to_count, countries):
+    print(champion_to_count)
+    print(countries)
 
 
-def get_file_information(filename):
-    file_information = []
+def get_records(filename):
+    records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline() # removes first line
+        in_file.readline()  # removes first line of information in text file
         for line in in_file:
             parts = line.strip().split(",")
-            file_information.append(parts)
-    return file_information
+            records.append(parts)
+    return records
 
 
 main()
