bf140f7b42faf359aa8e9a665a66e03e6d836ef6
KalynAnne <kalyn.barry@my.jcu.edu.au>
2023-07-02 20:07:19 +1000

    ADD EAFP approach

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,16 +8,25 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
+"""LOOK BEFORE YOU LEAP"""
+# state_code = input("Enter short state: ").upper()
+# while state_code != "":
+#     if state_code in CODE_TO_NAME:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
+
+
+"""EASIER TO ASK FORGIVENESS THAN PERMISSION"""
 state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
+try:
+    while state_code != "":
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+        state_code = input("Enter short state: ").upper()
+except KeyError:
+    print("Invalid short state")
+print("end")
 
-
-for code, name in CODE_TO_NAME.items():
-    print(f"{code:3} is {name}")
-
-
+# for code, name in CODE_TO_NAME.items():
+#     print(f"{code:3} is {name}")
