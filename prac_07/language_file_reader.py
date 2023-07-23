6a9938d3122b2510537688144bac222be5a853ef
KalynAnne <kalyn.barry@my.jcu.edu.au>
2023-07-23 23:01:36 +1000

    fix print issue

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -29,7 +29,7 @@ def main():
         pointer_arithmetic = parts[4] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, pointer_arithmetic, int(parts[3]))
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
@@ -64,7 +64,7 @@ def using_namedtuple():
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, Pointer Arithmetic, year')
     reader = csv.reader(in_file)  # use default dialect, Excel
 
     for row in reader:
@@ -79,7 +79,7 @@ def using_namedtuple():
 
 def using_csv_namedtuple():
     """Language file reader version using both csv module and named tuple."""
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, Pointer Arithmetic, year')
     in_file = open("languages.csv", "r")
     in_file.readline()
     for language in map(Language._make, csv.reader(in_file)):
